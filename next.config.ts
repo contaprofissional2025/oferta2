e9aee7ac0ae416b1504127a715596d31435482b3
contaprofissional2025 <contaprofissionalpedro2025@gmail.com>
2025-05-14 08:17:30 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  basePath: '/parte4/pagamento',
+  assetPrefix: '/parte4/pagamento/',
+};
+
+export default nextConfig;
